8e0b262d13fbbe2676d9c10e897c48aa56ddae5f
MohdumaR125 <umar231294@gmail.com>
2023-01-24 20:37:05 +0530

    board updated

--- Frontend/tic-tac-toe/src/Components/Board.jsx
+++ Frontend/tic-tac-toe/src/Components/Board.jsx
@@ -4,15 +4,15 @@ import Styles from "./Styles.module.css"
 const Board = () =>{
     return (
         <div className={Styles.board}>
-            <div>O</div>
-            <div>X</div>
-            <div>X</div>
-            <div>O</div>
-            <div>O</div>
-            <div>X</div>
-            <div>X</div>
-            <div>O</div>
-            <div>X</div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     )
 }
